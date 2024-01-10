9302211f37063e310cadaf17ae23e7047319b742
Irina <myr-irina2@yandex.ru>
2024-01-10 21:55:53 +0300

    fix: add solution to getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -507,9 +507,17 @@ console.log(getIdentityMatrix(5));
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((acc, curr) => {
+    if (curr % 2 !== 0) {
+      acc.push(numbers.indexOf(curr));
+    }
+
+    return acc;
+  }, []);
 }
+
+console.log(getIndicesOfOddNumbers([1, 2, 3, 4, 5]));
 
 /**
  * Returns the array of RGB Hex strings from the specified array of numbers.
