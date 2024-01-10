0677471661982abb4a428a19c3133029f0f5f3c8
Irina <myr-irina2@yandex.ru>
2024-01-10 16:41:34 +0300

    fix: add solution to calculateBalance task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -350,9 +350,11 @@ console.log(flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']));
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
+
+console.log(selectMany(['one', 'two', 'three'], (x) => x.split('')));
 
 /**
  * Every month, you record your income and expenses.
@@ -367,9 +369,18 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  if (!arr.length) return 0;
+
+  return arr.reduce((acc, [income, expenses]) => acc + (income - expenses), 0);
 }
+
+console.log(
+  calculateBalance([
+    [10, 8],
+    [5, 1],
+  ])
+);
 
 /**
  * Breaks an array into chunks of the specified size.
