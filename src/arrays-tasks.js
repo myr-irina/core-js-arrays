f6f7b71a22182b0fc35718a5128e036bd6dcbde6
Irina <myr-irina2@yandex.ru>
2024-01-12 11:28:58 +0300

    fix: add solution to shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -606,9 +606,13 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  if (!arr.length) return [];
+
+  return arr.map((item, index) => Array(index + 1).fill(item)).flat();
 }
+
+console.log(propagateItemsByPositionIndex(['a', 'b']));
 
 /**
  * Shifts an array by n positions. If n is negative, the array is shifted to the left;
@@ -623,9 +627,13 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return n > 0
+    ? [...arr.slice(-n), ...arr.slice(0, -n)]
+    : [...arr.slice(-n % arr.length), ...arr.slice(0, -n % arr.length)];
 }
+
+console.log(shiftArray([10, 20, 30, 40, 50], -3));
 
 /**
  * Sorts digit names.
